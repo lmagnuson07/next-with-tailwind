3a68dd1bbf0b755d19a5380f2fb569b58e0d76b9
lmagnuson07 <logmagns07@gmail.com>
2022-10-30 14:17:20 -0600

    updated lighthouse warnings

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
+import Head from 'next/head'
+
 import { PageHeading } from '../components/page-heading' 
 
 export default function Home() {
   return (
-    <PageHeading 
-      title="Deploying To Vercel"
-      tagline="setting up a continuous integration continous delivery workflow" 
-    />
+    <>
+      <Head>
+        <meta charSet="UTF-8"/>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="description" content="Tutorial on how to configure NEXT.js with tailwindcss."/>
+        <title>Learning React Vercel Deployment</title>
+      </Head>
+      <PageHeading 
+        title="Deploying To Vercel"
+        tagline="setting up a continuous integration continous delivery workflow" 
+      />
+    </>
+    
   )
 }
